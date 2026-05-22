53f74cdb0e904ccded271811ed93ec520f1dbdb0
Gloria Enyi <nkemaconam@gmail.com>
2026-05-22 22:32:14 +0100

    Fix PWA cache size limit

--- vite.config.ts
+++ vite.config.ts
@@ -17,7 +17,9 @@ export default defineConfig({
       registerType: 'autoUpdate',
 
       workbox: {
-        globPatterns: ['**/*.{js,css,html,ico,png,svg,woff,woff2}'],
+  maximumFileSizeToCacheInBytes: 3 * 1024 * 1024,
+
+  globPatterns: ['**/*.{js,css,html,ico,png,svg,woff,woff2}'],
 
         runtimeCaching: [
           {
