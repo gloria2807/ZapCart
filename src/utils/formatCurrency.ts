e196d68b31c18c43b8da4dd307aa7b623aad54b7
Gloria Enyi <nkemaconam@gmail.com>
2026-05-23 08:07:40 +0100

    updated sats to ngn conversion

--- src/utils/formatCurrency.ts
+++ src/utils/formatCurrency.ts
@@ -1,8 +1,8 @@
 export function formatSats(amount: number) {
   return `${amount} sats`;
 }
-const SATS_TO_NGN = 0.935251;
-const NGN_TO_SATS = 1 / 0.935251;
+const SATS_TO_NGN = 1381 / 1330;
+const NGN_TO_SATS = 1330 / 1381;
 
 export function convertNgnToSats(ngn: number) {
   return Math.round(ngn * NGN_TO_SATS);
